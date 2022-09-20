53a6f3d9f6608d26d9ea09d5bb3b0b21e629de28
Noah Hakansson <noah.hakansson@protonmail.com>
2022-09-20 19:12:28 +0200

    reverted this file to sequantial version.

--- matrix_inverse.c
+++ matrix_inverse.c
@@ -1,37 +1,25 @@
 /***************************************************************************
  *
  * Sequential version of Matrix Inverse
- * An adapted version of the code by Hkan Grahn
+ * An adapted version of the code by Håkan Grahn
  ***************************************************************************/
 
 #include <assert.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 #define MAX_SIZE 4096
 
 typedef double matrix[MAX_SIZE][MAX_SIZE];
 
-int N;            /* matrix size                */
+int N;            /* matrix size		*/
 int maxnum;       /* max number of element*/
-char *Init;       /* matrix init type   */
-int PRINT;        /* print switch               */
-matrix A;         /* matrix A           */
+char *Init;       /* matrix init type	*/
+int PRINT;        /* print switch		*/
+matrix A;         /* matrix A		*/
 matrix I = {0.0}; /* The A inverse matrix, which will be initialized to the
                      identity matrix */
-
-pthread_barrier_t barrier;
-struct threadArgs {
-  int id;
-  int p;
-  int col;
-  int row;
-  int pivalue;
-  int multiplier;
-};
 
 /* forward declarations */
 void find_inverse(void);
@@ -39,169 +27,20 @@ void Init_Matrix(void);
 void Print_Matrix(matrix M, char name[]);
 void Init_Default(void);
 int Read_Options(int, char **);
-void *child(void *params);
-void matrix_to_identity(int p, int col, double pivalue);
-void matrix_elimination(int p, int row, int col, double multiplier);
-void parallel_find_inverse(int col);
-void start_parallel_inversion(void);
 
 int main(int argc, char **argv) {
   printf("Matrix Inverse\n");
   int i, timestart, timeend, iter;
 
-  Init_Default();           /* Init default values      */
-  Read_Options(argc, argv); /* Read arguments   */
-  Init_Matrix();            /* Init the matrix  */
+  Init_Default();           /* Init default values	*/
+  Read_Options(argc, argv); /* Read arguments	*/
+  Init_Matrix();            /* Init the matrix	*/
+  find_inverse();
 
-  // Sequential
-  // printf("Starting Sequential\n");
-  // find_inverse();
-  // printf("Sequential done\n");
-
-  // Parallel
-  printf("Starting parallel\n");
-  start_parallel_inversion();
-  printf("Parallel done\n");
-
-  // print
   if (PRINT == 1) {
-    Print_Matrix(A, "End: Input");
+    // Print_Matrix(A, "End: Input");
     Print_Matrix(I, "Inversed");
   }
-}
-
-void *child(void *params) {
-  struct threadArgs *args = (struct threadArgs *)params;
-  int id = args->id;
-  int p = args->p;
-  int col = args->col;
-  int row = args->row;
-  int pivalue = args->pivalue;
-  int multiplier = args->multiplier;
-  // printf("Proccess %d starting work...\n", id);
-  matrix_to_identity(p, id, pivalue);
-  pthread_barrier_wait(&barrier);
-  free(args);
-  // printf("Proccess %d done and freed.\n", id);
-  return NULL;
-}
-
-void *start_parallel_identity(void *params) {
-  struct threadArgs *args = (struct threadArgs *)params;
-  int id = args->id;
-  int p = args->p;
-  int col = args->col;
-  int row = args->row;
-  int pivalue = args->pivalue;
-  int multiplier = args->multiplier;
-  // printf("Proccess %d starting work...\n", id);
-  matrix_to_identity(p, id, pivalue);
-  pthread_barrier_wait(&barrier);
-  free(args);
-  // printf("Proccess %d done and freed.\n", id);
-  return NULL;
-}
-
-void *start_parallel_elimination(void *params) {
-  struct threadArgs *args = (struct threadArgs *)params;
-  int id = args->id;
-  int p = args->p;
-  int col = args->col;
-  int row = args->row;
-  int pivalue = args->pivalue;
-  int multiplier = args->multiplier;
-  // printf("Proccess %d starting work...\n", id);
-  matrix_elimination(p, row, col, multiplier);
-  pthread_barrier_wait(&barrier);
-  free(args);
-  // printf("Proccess %d done and freed.\n", id);
-  return NULL;
-}
-
-void start_parallel_inversion() {
-  struct threadArgs *args;
-
-  pthread_t *children;
-  int id = 0;
-  pthread_barrier_init(&barrier, NULL, N);
-
-  children = malloc(N * sizeof(pthread_t));
-  for (id = 0; id < N; id++) {
-    args = malloc(sizeof(struct threadArgs));
-    args->id = id;
-    args->col = id;
-    pthread_create(&(children[id]), NULL, child, (void *)args);
-  }
-  for (id = 0; id < N; id++) {
-    pthread_join(children[id], NULL);
-  }
-  free(children);
-}
-
-void start_children(void *workerFunc, struct threadArgs *args) {
-  pthread_t *children;
-  int id = 0;
-  pthread_barrier_init(&barrier, NULL, N);
-
-  children = malloc(N * sizeof(pthread_t));
-  for (id = 0; id < N; id++) {
-    args = malloc(sizeof(struct threadArgs));
-    args->id = id;
-    args->col = id;
-    pthread_create(&(children[id]), NULL, workerFunc, (void *)args);
-  }
-  for (id = 0; id < N; id++) {
-    pthread_join(children[id], NULL);
-  }
-  free(children);
-}
-
-void parallel_find_inverse(int col) {
-  int row, p;     // 'p' stands for pivot (numbered from 0 to N-1)
-  double pivalue; // pivot value
-
-  /* Bringing the matrix A to the identity form */
-  for (p = 0; p < N; p++) { /* Outer loop */
-    pivalue = A[p][p];
-    // TODO: Code to start and join threads for matrix_to_identity.
-    struct threadArgs *identityArgs;
-    identityArgs->pivalue = pivalue;
-    identityArgs->p = p;
-    start_children(start_parallel_identity, identityArgs);
-    // printf("thread %d hit first barrier\n", col);
-    pthread_barrier_wait(&barrier);
-    assert(A[p][p] == 1.0);
-    // Print_Matrix(A, "End: Input");
-    // Print_Matrix(I, "Inversed");
-
-    // Elimination
-    double multiplier;
-    for (row = 0; row < N; row++) {
-      multiplier = A[row][p];
-      if (row != p) // Perform elimination on all except the current pivot row
-      {
-        // TODO: Code to start and join threads for matrix_elimination.
-        struct threadArgs *eliminationArgs;
-        eliminationArgs->multiplier = multiplier;
-        eliminationArgs->p = p;
-        start_children(start_parallel_elimination, identityArgs);
-        // printf("thread %d hit second barrier\n", col);
-        assert(A[row][p] == 0.0);
-      }
-    }
-  }
-}
-
-void matrix_to_identity(int p, int col, double pivalue) {
-  A[p][col] = A[p][col] / pivalue; /* Division step on A */
-  I[p][col] = I[p][col] / pivalue; /* Division step on I */
-}
-
-void matrix_elimination(int p, int row, int col, double multiplier) {
-  A[row][col] =
-      A[row][col] - A[p][col] * multiplier; /* Elimination step on A */
-  I[row][col] =
-      I[row][col] - I[p][col] * multiplier; /* Elimination step on I */
 }
 
 void find_inverse() {
@@ -216,10 +55,7 @@ void find_inverse() {
       I[p][col] = I[p][col] / pivalue; /* Division step on I */
     }
     assert(A[p][p] == 1.0);
-    Print_Matrix(A, "End: Input");
-    Print_Matrix(I, "Inversed");
 
-    // Elimination
     double multiplier;
     for (row = 0; row < N; row++) {
       multiplier = A[row][p];
@@ -251,7 +87,7 @@ void Init_Matrix() {
 
   printf("\nsize      = %dx%d ", N, N);
   printf("\nmaxnum    = %d \n", maxnum);
-  printf("Init    = %s \n", Init);
+  printf("Init	  = %s \n", Init);
   printf("Initializing matrix...");
 
   if (strcmp(Init, "rand") == 0) {
@@ -317,12 +153,12 @@ int Read_Options(int argc, char **argv) {
         exit(0);
         break;
       case 'u':
-        printf("\nUsage: matinv [-n problemsize]\n \
-          [-D] show default values \n \
-          [-h] help \n \
-          [-I init_type] fast/rand \n \
-          [-m maxnum] max random no \n \
-          [-P print_switch] 0/1 \n");
+        printf("\nUsage: matinv [-n problemsize]\n");
+        printf("           [-D] show default values \n");
+        printf("           [-h] help \n");
+        printf("           [-I init_type] fast/rand \n");
+        printf("           [-m maxnum] max random no \n");
+        printf("           [-P print_switch] 0/1 \n");
         exit(0);
         break;
       case 'D':
