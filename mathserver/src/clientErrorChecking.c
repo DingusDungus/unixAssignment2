8bea5822fe0e3d33b8c613c50e306e31a8c1c48e
DingusDungus <DingusDungus@github.com>
2022-12-01 12:03:41 +0100

    Cleaned up debug prints

--- mathserver/src/clientErrorChecking.c
+++ mathserver/src/clientErrorChecking.c
@@ -1,26 +1,18 @@
 #include "../include/clientErrorChecking.h"
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
 // Checks if argv meet requirements for matinv
-bool matinvCheck(char argv[10][100])
-{
-  for (int i = 1;i < 10 && argv[i][0] != 0;i++)
-  {
-    if (strcmp(argv[i], "-n") == 0)
-    {
-      printf("-n found\n");
-      if ((i + 1) >= 10 || !stringIsNumber(argv[i+1]))
-      {
+bool matinvCheck(char argv[10][100]) {
+  for (int i = 1; i < 10 && argv[i][0] != 0; i++) {
+    if (strcmp(argv[i], "-n") == 0) {
+      if ((i + 1) >= 10 || !stringIsNumber(argv[i + 1])) {
         printf("Error; No value given to -n, ex. -n 9\n");
         return false;
       }
-    }
-    else if (strcmp(argv[i], "-I") == 0)
-    {
-      printf("-I found\n");
-      if ((i + 1) >= 10 || ((strcmp(argv[i + 1], "rand") != 0 && strcmp(argv[i + 1], "fast") != 0)))
-      {
+    } else if (strcmp(argv[i], "-I") == 0) {
+      if ((i + 1) >= 10 || ((strcmp(argv[i + 1], "rand") != 0 &&
+                             strcmp(argv[i + 1], "fast") != 0))) {
         printf("Error; No value given to -I (acceptable \"rand\", \"fast\")\n");
         return false;
       }
@@ -30,27 +22,20 @@ bool matinvCheck(char argv[10][100])
 }
 
 // Checks if argv meet requirements for kmeans
-bool kmeansCheck(char argv[10][100])
-{
+bool kmeansCheck(char argv[10][100]) {
   bool fFlagGiven = false;
-  for (int i = 1;i < 10 && argv[i][0] != 0;i++)
-  {
-    if (strcmp(argv[i], "-k") == 0)
-    {
-      if ((i + 1) >= 10 || !stringIsNumber(argv[i+1]))
-      {
+  for (int i = 1; i < 10 && argv[i][0] != 0; i++) {
+    if (strcmp(argv[i], "-k") == 0) {
+      if ((i + 1) >= 10 || !stringIsNumber(argv[i + 1])) {
         printf("Error; -k flag is not given a number\n");
         return false;
       }
       continue;
     }
 
-    if (strcmp(argv[i], "-f") == 0)
-    {
-      if ((i + 1) < 10 && argv[i+1][0] != 0)
-      {
-        if (!fileExists(argv[i + 1]))
-        {
+    if (strcmp(argv[i], "-f") == 0) {
+      if ((i + 1) < 10 && argv[i + 1][0] != 0) {
+        if (!fileExists(argv[i + 1])) {
           printf("Error; Given file does not exist!\n");
           return false;
         }
@@ -72,16 +57,11 @@ bool kmeansCheck(char argv[10][100])
 bool checkCommandValidity(char *command) {
   char argv[10][100];
   transformIntoArgV(command, argv);
-  if (strcmp(argv[0], "matinv") == 0)
-  {
+  if (strcmp(argv[0], "matinv") == 0) {
     return matinvCheck(argv);
-  }
-  else if (strcmp(argv[0], "kmeans") == 0)
-  {
+  } else if (strcmp(argv[0], "kmeans") == 0) {
     return kmeansCheck(argv);
-  }
-  else if (strcmp(argv[0], "done") == 0)
-  {
+  } else if (strcmp(argv[0], "done") == 0) {
     return true;
   }
   return false;
